ebf65c84575ec3d36e58163164779b68d18d0c2e
Aridaitech <55482302+aridaisantana@users.noreply.github.com>
2026-08-25 16:50:56 +0200

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let numsWithFrequency = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            if (!numsWithFrequency.has(nums[i])) {
+                numsWithFrequency.set(nums[i], 1);
+            } else {
+                numsWithFrequency.set(nums[i], numsWithFrequency.get(nums[i]) + 1);
+            }
+        }
+        let maxFrequencies = new Array(nums.length + 1);
+        for (const [key, value] of numsWithFrequency) {
+            if (maxFrequencies[value]) {
+                maxFrequencies[value].push(key);
+            } else {
+                maxFrequencies[value] = [key];
+            }
+        }
+        let result = new Array();
+        for(let i = maxFrequencies.length - 1; i >= 0; i--){
+            if(maxFrequencies[i]){
+                result.push(...maxFrequencies[i])
+
+                if(result.length >= k){
+                    return result.slice(0, k)
+                }
+            }
+        }
+    }
+}
