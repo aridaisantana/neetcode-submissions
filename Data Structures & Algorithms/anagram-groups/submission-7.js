65ec37067a62bd36a8b72a1f6aacecde700eab4e
Aridaitech <55482302+aridaisantana@users.noreply.github.com>
2026-08-25 15:40:09 +0200

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let wordAndKey = new Map();
+        for (let i = 0; i < strs.length; i++) {
+            let canonicalRepresentation = new Array(26).fill(0);
+            for (let j = 0; j < strs[i].length; j++) {
+                canonicalRepresentation[strs[i].charAt(j).charCodeAt(0) - 97]++;
+            }
+            const key = canonicalRepresentation.join("#");
+            if (!wordAndKey.has(key)) {
+                wordAndKey.set(key, [strs[i]]);
+            } else {
+                wordAndKey.set(key, [...wordAndKey.get(key), strs[i]]);
+            }
+        }
+        return Array.from(wordAndKey.values());
+    }
+}
